48616dbe6b2a85c4a8d25914aa77b5cd770b3686
Xavier <gaskin.xavier@gmail.com>
2020-02-19 11:23:20 -0500

    X- Room gen.+

---
+++ UnknownEntityUnity/Assets/Scripts/ScriptableObjects/LevelCreation/SO_LevelBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Level", menuName = "Level")]
+public class SO_LevelBase : ScriptableObject
+{
+    public int minRoomAmnt, maxRoomAmnt;
+    public int minRoomSize, maxRoomSize;
+    public List<SO_RoomBase> rooms = new List<SO_RoomBase>();
+    public int difficulty;
+}
---
+++ UnknownEntityUnity/Assets/Scripts/ScriptableObjects/LevelCreation/SO_RoomBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Room", menuName = "Room")]
+public class SO_RoomBase : ScriptableObject
+{
+    public Vector3[] spawnPoints;
+    public Vector3[] roomConnections;
+
+}
