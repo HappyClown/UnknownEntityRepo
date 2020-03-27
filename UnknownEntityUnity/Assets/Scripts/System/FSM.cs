446a959759641661485bd5839445701168694d89
Xavier <gaskin.xavier@gmail.com>
2020-03-26 21:12:13 -0400

    X- FSM base, enemyFSMGraph psd

---
+++ UnknownEntityUnity/Assets/Scripts/System/FSM.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM : MonoBehaviour
+{
+    public delegate void Del();
+    public Del activeState;
+
+    public void SetActiveState(Del newState) {
+        activeState = newState;
+    }
+
+    public void FSMUpdate() {
+        if (activeState != null) {
+            activeState();
+        }
+    }
+}
